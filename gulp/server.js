1ce0925c4665ae0b20a4b07e00270283e8882f41
r_zou <r_zou@Ctrip.com>
2015-03-25 18:34:45 +0800

    server.js

--- gulp/server.js
+++ gulp/server.js
@@ -3,7 +3,6 @@ module.exports = function (gulp) {
     var gulp = gulp;
     var browserSync = require('browser-sync');
     var sass        = require('gulp-sass');
-    var reload      = browserSync.reload;
     var nodemon = require('gulp-nodemon');
     var inject = require('gulp-inject');
 
@@ -14,7 +13,6 @@ module.exports = function (gulp) {
                 ignorePath: 'app'
             }))
             .pipe(gulp.dest('app'))
-            //.pipe(reload({stream: true}));
     });
 
     gulp.task('app', ['html-inject','sass','nodemon'] , function () {
@@ -40,24 +38,6 @@ module.exports = function (gulp) {
         return gulp.src(['app/views/scss/*.scss'])
             .pipe(sass())
             .pipe(gulp.dest("app/views/css"))
-            //.pipe(reload({stream: true}));
     });
-
-    //var server = require('gulp-develop-server');
-    //var plugins = require('gulp-load-plugins')();
-    //
-    //// 启动
-    //gulp.task('server', ['server', 'server:restart'], function () {
-    //    console.log('express server start');
-    //});
-    //// start express server
-    //gulp.task('server', function () {
-    //    server.listen({path: 'app/app.js'});
-    //});
-    //
-    //// restart server if app.js changed
-    //gulp.task('server:restart', function () {
-    //    gulp.watch(['./nodejs/app.js', './nodejs/router.js', './nodejs/controllers/*.js', './nodejs/models/*.js', './nodejs/middlewares/*.js', './nodejs/views/*.hbs', './nodejs/views/partials/*.hbs'], server.restart);
-    //});
 };
 
